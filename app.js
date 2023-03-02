ebfa8cf91eb11c586616d88614568d4e79b6ade9
ronanfilloy <ronanfilloy@gmail.com>
2023-03-02 19:33:09 -0300

    changed app

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ const reviewRoutes = require("./routes/reviews");
 const userRoutes = require("./routes/users");
 
 // "mongodb://localhost:27017/yelp-camp"
-
 const dbUrl = process.env.DB_URL;
 
 mongoose.connect(dbUrl);
