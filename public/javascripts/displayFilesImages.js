92d5474bff2a16de39dbbdab39347f5511ba32b8
ronanfilloy <ronanfilloy@gmail.com>
2023-03-06 15:15:54 -0300

    added image preview when uploading images

--- public/javascripts/displayFilesImages.js
+++ public/javascripts/displayFilesImages.js
@@ -1,11 +1,14 @@
 function previewMultiple (event) {
     const form = document.querySelector("#formFile");
     form.innerHTML = "";
-    var images = document.getElementById("image");
-    var number = images.files.length;
-    for(i = 0; i < number; i++) {
-        var urls = URL.createObjectURL(event.target.files[i]);
-        document.getElementById("formFile").innerHTML += "<img src='" + urls + "'>";
+    for(i = 0; i < event.target.files.length; i++) {
+        const urls = URL.createObjectURL(event.target.files[i]);
+        document.getElementById("formFile").innerHTML += `<img src="${urls}">`;
     }
 };
+
+document.querySelector("#imageUpload").addEventListener("change", (ev) => {
+    if(!ev.target.files) return;
+    previewMultiple(ev);
+});
 
